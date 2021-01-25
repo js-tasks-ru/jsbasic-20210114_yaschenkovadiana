38bc229960acb180437f667440cb906017ed093e
Diaa Yashenkova <diana1_95@inbox.ru>
2021-01-25 13:45:18 +0900

    Update index.js

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
-  // ваш код
+  a:0,
+  b:0,  
+  read (a,b) {
+     this.a = prompt("Значение a ", " ");
+     this.b = prompt("Значение b ", " ");
+      },
+  sum () {
+    alert (this.a + this.b);
+  },
+  mul () {
+   alert (this.a * this.b); 
+  } 
+  
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
