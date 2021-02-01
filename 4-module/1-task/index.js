8cbbaa25eb3687b14b4f0c0cdeb99f2d5fd232a4
Diaa Yashenkova <diana1_95@inbox.ru>
2021-02-01 16:48:38 +0900

    Update index.js

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let fragment = new DocumentFragment();
+  let ul = document.createElement('ul');
+  fragment.append(ul);
+  
+  for(let i in friends) {
+    let friend = friends[i].firstName + " " + friends[i].lastName;
+    let li = document.createElement('li');
+    li.append(friend);
+    ul.prepend(li);
+   }
+  
+   return fragment;
 }
