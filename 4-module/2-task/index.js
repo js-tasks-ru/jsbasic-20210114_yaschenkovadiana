f7ca927dd6eaec621c99c5ff22fc25e03097dc39
Diaa Yashenkova <diana1_95@inbox.ru>
2021-02-03 14:46:30 +0800

    Update index.js

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -3,4 +3,9 @@
  * @return {void}
  */
 function makeDiagonalRed(table) {
+    for (let i = 0; i<=5; i++) {
+        let td = table.rows[i].cells[i];
+        td.style.backgroundColor = 'red';
+        return td;
+      }
 }
