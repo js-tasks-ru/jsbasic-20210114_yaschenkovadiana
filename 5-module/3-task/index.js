04513d9768921cb6d1c267964f89001f83ce9613
Diaa Yashenkova <diana1_95@inbox.ru>
2021-02-06 22:07:27 +0900

    Update index.js

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,14 @@
 function initCarousel() {
-  // ваш код...
+      //класс с каруселью
+      let carousel = document.querySelector('.carousel__inner');
+      //размер слайда
+      let widthCarousel = carousel.offsetWidth + 'px';
+      
+    
+    let next = document.querySelector('.carousel__arrow carousel__arrow_right');
+    let prev = document.querySelector('.carousel__arrow carousel__arrow_left');
+    prev.oneClick = function () {
+      carousel.style.transform = 'translateX(-widthCarousel)';
+    }
+      
 }
