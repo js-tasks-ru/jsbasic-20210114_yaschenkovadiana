6abdb7037edcedfaa25a514e3b2cd1c3db8a3400
Diaa Yashenkova <diana1_95@inbox.ru>
2021-02-08 17:06:20 +0900

    1

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+
+  button.onclick = function() {
+    let style = text.style.display;
+      if (style === "") {
+        text.style.display = 'none'; 
+       }
+       else if (style === 'none') {
+        text.style.display = ""; 
+      }
+}; 
 }
